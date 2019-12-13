cbc9c471d3d67de3df5b0173187c919f3985b4f2
Author Name <kshin14@binghamton.edu>
2019-12-13 06:51:38 -0500

    added pictures and started search

---
+++ frontend/components/search_bar/search_bar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+class SearchBar extends React.Component {
+
+
+
+
+}
+
+export default Search;
---
+++ frontend/components/search_bar/search_container.jsx
@@ -0,0 +1,17 @@
+import {connect} from 'react-redux';
+
+import SearchBar from './search_bar';
+
+const msp = state => {
+  return {
+
+  }
+}
+
+const mdp = dispatch => {
+  return {
+
+  }
+}
+
+export default connect(msp, mdp)(search)
