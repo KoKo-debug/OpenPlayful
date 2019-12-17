2229b517fa699ce6e48d7095d83be58ba94032d9
Korrey Shin <kshin14@binghamton.edu>
2019-12-17 10:58:59 -0500

    show and index done?

--- frontend/components/cafes/cafe_show.jsx
+++ frontend/components/cafes/cafe_show.jsx
@@ -10,7 +10,7 @@ class CafeShow extends React.Component {
     this.photoRef = React.createRef();
     this.menuRef = React.createRef();
     this.reviewRef = React.createRef();
-    this.scrollToOverview = this.scrollToOverview.bind(this);
+    // this.scrollToOverview = this.scrollToOverview.bind(this);
   }
 
 
@@ -22,35 +22,42 @@ class CafeShow extends React.Component {
 
   scrollToOverview(ref) {
     return e => {
-      if (ref) {
-        ref.current.scrollIntoView(true)
+      let y;
+      if (ref.current.id) { 
+        y = ref.current.getBoundingClientRect().top + window.pageYOffset - 58;
       }
-    }
+      window.scrollTo(0, y);
+    };
   }
 
+  scrollToPhoto(ref) {
+    return e => {
+      let y;
+      if (ref.current.id) {
+        y = ref.current.getBoundingClientRect().top + window.pageYOffset - 58;
+      }
+      window.scrollTo(0, y);
+    };
+  }
 
-  // scrollToOverview(ref) {
-  //   //65 is height of nav bar;
-  //   let y;
-  //   if (ref.current.id) { 
-  //     y = ref.current.getBoundingClientRect().top + window.pageYOffset - 65;
-  //   }
-
-  //   window.scrollTo(0, y)
-  // }
-
+  scrollToMenu(ref) {
+    return e => {
+      let y;
+      if (ref.current.id) {
+        y = ref.current.getBoundingClientRect().top + window.pageYOffset - 58;
+      }
+      window.scrollTo(0, y);
+    };
+  }
   
-
-  scrollToPhoto() {
-    this.photoRef.current.scrollIntoView(true)
-  }
-
-  scrollToMenu() {
-    this.menuRef.current.scrollIntoView(true)
-  }
-
-  scrollTOReview() {
-    this.reviewRef.current.scrollIntoView(true)
+  scrollToReview(ref) {
+    return e => {
+      let y;
+      if (ref.current.id) {
+        y = ref.current.getBoundingClientRect().top + window.pageYOffset - 58;
+      }
+      window.scrollTo(0, y);
+    };
   }
 
   render() {
@@ -59,69 +66,55 @@ class CafeShow extends React.Component {
 
     if (!this.props.cafe) return null;
 
-    const {cafe} = this.props
+    const {cafe} = this.props;
 
     const { cost, animal, average_rating, description, number_reviews, phone_number, url } = this.props.cafe
 
     const price = (cost) => {
       if (cost === 2) {
-        return "$30 and under"
+        return "$30 and under";
       } else if (cost === 3) {
-        return "$31 to $50"
+        return "$31 to $50";
       } else {
-        return "$50 and over"
+        return "$50 and over";
       }
-    }
+    };
 
     const photoLis = cafe.url.map((url, id) =>
       <div className="photo-gallery">
-        <img className="gallery-images" key={id} src={url} width="100" height="300" />
+        <img className="gallery-images" key={id} src={url} />
       </div>
     );
-    
-    debugger;
+    let randomUrl = cafe.url[Math.floor(Math.random() * cafe.url.length)]
+    const bannerPhoto = <img className="bannerPhoto" src={randomUrl} />
     return(
       <div>
 
-      <section className="cafe-show-outer-container">
-      </section>
+        <section className="cafe-show-banner-container">
+          {bannerPhoto}
+        </section>
         <section className="cafe-show-info-container">
           <div className="cafe-navBar">
-            {/* if HashLink is okay */}
             <div
               onClick={this.scrollToOverview(this.overviewRef)}
               className="cafe-nav overview">
                 Overview
             </div>
-
-            {/* For hashLink
-
-            Window.scrollTo(0, 200)
-
-            Function()
-
-            Y = menu.height + overview.height
-
-            window.scrollTo (0, y) */}
-
-            <HashLink
-              to={`/cafes/${cafe.id}#photos`}
+            <div
+              onClick={this.scrollToPhoto(this.photoRef)}
               className="cafe-nav photos">
                 Photos
-            </HashLink>
-
-            <HashLink
-              to={`/cafes/${cafe.id}#menu`}
+            </div>
+            <div
+              onClick={this.scrollToMenu(this.menuRef)}
               className="cafe-nav menu">
                 Menu
-            </HashLink>
-
-
-            <HashLink
-              to={`/cafes/${cafe.id}#reviews`}
+            </div>
+            <div
+              onClick={this.scrollToReview(this.reviewRef)}
               className="cafe-nav reviews">
                 Reviews
-            </HashLink>
+            </div>
           </div>
           
           <div id="overview" ref={this.overviewRef}>
@@ -136,9 +129,7 @@ class CafeShow extends React.Component {
                     {3.2}
                   </h3>
               </div>
-              
 
-      
               <div className="num-reviews">
                 <div>
                   {number_reviews}
@@ -158,45 +149,78 @@ class CafeShow extends React.Component {
             <div className="description">
               {description}
             </div>
+          </div>
 
-            <div id="photos">
-              <h1 className="photos-header">{url.length} Photos</h1>
-              <section className="photo-list">
-                {photoLis}
-              </section>
-            </div>
+          <div id="photos" ref={this.photoRef}>
+            <h1 className="photos-header">{url.length} Photos</h1>
+            <section className="photo-list">
+              {photoLis}
+            </section>
+          </div>
 
-            <div id="menu">
-              <h1 className="menu-header">
-                Menu
-              </h1>
-              <Link to="" className="menu-items">
-                View menu on restaurant's website
-              </Link>
-            </div>
+          <div id="menu" ref={this.menuRef}>
+            <h1 className="menu-header">
+              Menu
+            </h1>
+            <Link to={`/cafes/${cafe.id}`} className="menu-items">
+              View menu on restaurant's website
+            </Link>
+          </div>
 
-            <div id="reviews">
-              <h1 className="review-header">
-                What {number_reviews} People Are Saying
-              </h1>
+          <div id="reviews" ref={this.reviewRef}>
+            <h1 className="review-header">
+              What {number_reviews} People Are Saying
+            </h1>
+
+            <section>
               <div className="reviews-info-container">
                 <h2>
                   Overall ratings and reviews
                 </h2>
-
-                <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga minima doloribus aperiam corporis animi aliquid, quisquam saepe ea incidunt minus reprehenderit molestiae accusamus, ipsum vel voluptas explicabo, ex voluptatem perspiciatis?
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus explicabo sapiente ab fugiat quo at mollitia architecto iste eveniet, molestiae autem quod molestias saepe deserunt reiciendis eius maxime temporibus dolorum?
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore sequi omnis explicabo veniam, id error adipisci amet fuga qui autem, culpa ea hic nostrum eum illo harum? Aut, praesentium maiores?
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, temporibus? Saepe libero ratione aliquid iure ut illo soluta eveniet, laborum accusantium aspernatur quidem fuga error nobis alias, ipsum facilis ducimus?
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore beatae debitis doloremque sit, nisi repudiandae voluptas saepe nemo non nostrum laboriosam voluptatum ullam cupiditate aut sint tempore sequi, vitae dicta.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, eaque, ullam praesentium quasi tempora nobis omnis eum aspernatur vero veniam reprehenderit nam accusamus voluptatibus esse itaque dolores sapiente commodi minus!
-                </p>
+                <h3>
+                  Reviews can only be made by diners who have eaten at this restaurant.
+                </h3>
               </div>
+            </section>
+            <section>
+              <div className="sort-by-dropdownn">
+                <h1>Sort by</h1>
+              </div>
+              <div className="review-filters">
+                <h1>Filters</h1>
+              </div>
+              <section className="reviewers" >
+                <section className="user-info">
+                  <div>
+                    user icon
+                  </div>
+                  <div>
+                    primary location
+                  </div>
+                  <div>
+                    Num Reviews
+                  </div>
+                </section>
+                <section>
+                  <section>
+                      <Ratings average_rating={3} red={red} />
+                      Dined someNum ago
+                  </section>
+                  <div>
+                    Overall * Food * Service * Ambience *
+                  </div>
+                  <div>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga minima doloribus aperiam corporis animi aliquid, quisquam saepe ea incidunt minus reprehenderit molestiae accusamus, ipsum vel voluptas explicabo, ex voluptatem perspiciatis?
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus explicabo sapiente ab fugiat quo at mollitia architecto iste eveniet, molestiae autem quod molestias saepe deserunt reiciendis eius maxime temporibus dolorum?
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore sequi omnis explicabo veniam, id error adipisci amet fuga qui autem, culpa ea hic nostrum eum illo harum? Aut, praesentium maiores?
+                      ctetur adipisicing elit. Quo, eaque, ullam praesentium quasi tempora nobis omnis eum aspernatur vero veniam reprehenderit nam accusamus voluptatibus esse itaque dolores sapiente commodi minus!
+                    </p>
+                  </div>
+                </section>
+              </section>
+            </section>
             </div>
-
-          </div>
-       
         </section>
       </div>
     )
